25807eb19ae41f585e071116e0893a17a342f1e4
janick-brischle <janick.brischle@de.markant.com>
2023-03-30 23:30:17 +0200

    update ngsw-config.json and enable service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -13,7 +13,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
               BrowserModule,
               NgOptimizedImage,
               ServiceWorkerModule.register('ngsw-worker.js', {
-                // enabled: !isDevMode(),
+                enabled: !isDevMode(),
                 // Register the ServiceWorker as soon as the application is stable
                 // or after 30 seconds (whichever comes first).
                 registrationStrategy: 'registerWhenStable:30000'
