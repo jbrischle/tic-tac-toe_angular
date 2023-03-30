8b7405e83c9e1e2c63c6caa7d54ab328b5a84064
janick-brischle <janick.brischle@de.markant.com>
2023-03-30 10:27:36 +0200

    make it a pwa

--- angular/tic-tac-toe/src/app/app.module.ts
+++ angular/tic-tac-toe/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgOptimizedImage } from '@angular/common';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-            imports: [
-              BrowserModule,
-              NgOptimizedImage
+            declarations: [
+              AppComponent
             ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+            imports:      [
+              BrowserModule,
+              NgOptimizedImage,
+              ServiceWorkerModule.register('ngsw-worker.js', {
+                // enabled: !isDevMode(),
+                // Register the ServiceWorker as soon as the application is stable
+                // or after 30 seconds (whichever comes first).
+                registrationStrategy: 'registerWhenStable:30000'
+              })
+            ],
+            providers:    [],
+            bootstrap:    [AppComponent]
+          })
+export class AppModule {
+}
