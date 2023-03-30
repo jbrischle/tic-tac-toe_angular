ad0936ebb0d15788ebd94c44081b54f1cc1e7612
janick-brischle <janick.brischle@de.markant.com>
2023-03-30 22:53:48 +0200

    better reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,120 +1,116 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 type icon = 'circle' | 'x';
 type cell = icon | null;
 type result = icon | 'draw' | null
+type game = { player: icon, field: cell[], playWon: result };
 
 @Component({
-             selector:    'app-root',
-             templateUrl: './app.component.html',
-             styleUrls:   ['./app.component.scss']
+             selector:        'app-root',
+             templateUrl:     './app.component.html',
+             styleUrls:       ['./app.component.scss'],
+             changeDetection: ChangeDetectionStrategy.OnPush
            })
 export class AppComponent {
-  private readonly DEFAULT_PLAYER: icon = 'circle';
-  player: icon = this.DEFAULT_PLAYER;
-  private readonly DEFAULT_FIELD: cell[] = [null, null, null, null, null, null, null, null, null];
-  field: cell[] = this.DEFAULT_FIELD;
-  private readonly DEFAULT_PLAYWON: result = null;
-  playWon: result = this.DEFAULT_PLAYWON;
+  game: game = this.resetGame();
 
   onClick(field: number) {
-    if (this.playWon) {
+    if (this.game.playWon) {
       return;
     }
 
-    if (this.field[field]) {
+    if (this.game.field[field]) {
       return;
     }
 
-    this.field[field] = this.player;
-
-    if (this.player === 'circle') {
-      this.player = 'x';
-    } else {
-      this.player = 'circle';
-    }
-
+    this.game.field[field] = this.game.player;
+    this.game.player = this.game.player === 'circle' ? 'x' : 'circle';
     this.checkGameEndCondition();
   }
 
   onReset(): void {
-    this.player = this.DEFAULT_PLAYER;
-    this.field = this.DEFAULT_FIELD;
-    this.playWon = this.DEFAULT_PLAYWON;
+    this.game = this.resetGame();
   }
 
-  private checkGameEndCondition() {
-    if ((this.field[0] === 'circle' &&
-         this.field[3] === 'circle' &&
-         this.field[6] === 'circle')
+  private resetGame(): game {
+    const DEFAULT_GAME = {
+      player: 'circle', field: [null, null, null, null, null, null, null, null, null], playWon: null
+    };
+    return JSON.parse(JSON.stringify(DEFAULT_GAME));
+  }
+
+  private checkGameEndCondition(): void {
+    if ((this.game.field[0] === 'circle' &&
+         this.game.field[3] === 'circle' &&
+         this.game.field[6] === 'circle')
         ||
-        (this.field[1] === 'circle' &&
-         this.field[4] === 'circle' &&
-         this.field[7] === 'circle')
+        (this.game.field[1] === 'circle' &&
+         this.game.field[4] === 'circle' &&
+         this.game.field[7] === 'circle')
         ||
-        (this.field[2] === 'circle' &&
-         this.field[5] === 'circle' &&
-         this.field[8] === 'circle')
+        (this.game.field[2] === 'circle' &&
+         this.game.field[5] === 'circle' &&
+         this.game.field[8] === 'circle')
         ||
-        (this.field[0] === 'circle' &&
-         this.field[1] === 'circle' &&
-         this.field[2] === 'circle')
+        (this.game.field[0] === 'circle' &&
+         this.game.field[1] === 'circle' &&
+         this.game.field[2] === 'circle')
         ||
-        (this.field[3] === 'circle' &&
-         this.field[4] === 'circle' &&
-         this.field[5] === 'circle')
+        (this.game.field[3] === 'circle' &&
+         this.game.field[4] === 'circle' &&
+         this.game.field[5] === 'circle')
         ||
-        (this.field[6] === 'circle' &&
-         this.field[7] === 'circle' &&
-         this.field[8] === 'circle')
+        (this.game.field[6] === 'circle' &&
+         this.game.field[7] === 'circle' &&
+         this.game.field[8] === 'circle')
         ||
-        (this.field[0] === 'circle' &&
-         this.field[4] === 'circle' &&
-         this.field[8] === 'circle')
+        (this.game.field[0] === 'circle' &&
+         this.game.field[4] === 'circle' &&
+         this.game.field[8] === 'circle')
         ||
-        (this.field[2] === 'circle' &&
-         this.field[4] === 'circle' &&
-         this.field[6] === 'circle')) {
-      this.playWon = 'circle';
+        (this.game.field[2] === 'circle' &&
+         this.game.field[4] === 'circle' &&
+         this.game.field[6] === 'circle')) {
+      this.game.playWon = 'circle';
       return;
     }
-    if ((this.field[0] === 'x' &&
-         this.field[3] === 'x' &&
-         this.field[6] === 'x')
+    if ((this.game.field[0] === 'x' &&
+         this.game.field[3] === 'x' &&
+         this.game.field[6] === 'x')
         ||
-        (this.field[1] === 'x' &&
-         this.field[4] === 'x' &&
-         this.field[7] === 'x')
+        (this.game.field[1] === 'x' &&
+         this.game.field[4] === 'x' &&
+         this.game.field[7] === 'x')
         ||
-        (this.field[2] === 'x' &&
-         this.field[5] === 'x' &&
-         this.field[8] === 'x')
+        (this.game.field[2] === 'x' &&
+         this.game.field[5] === 'x' &&
+         this.game.field[8] === 'x')
         ||
-        (this.field[0] === 'x' &&
-         this.field[1] === 'x' &&
-         this.field[2] === 'x')
+        (this.game.field[0] === 'x' &&
+         this.game.field[1] === 'x' &&
+         this.game.field[2] === 'x')
         ||
-        (this.field[3] === 'x' &&
-         this.field[4] === 'x' &&
-         this.field[5] === 'x')
+        (this.game.field[3] === 'x' &&
+         this.game.field[4] === 'x' &&
+         this.game.field[5] === 'x')
         ||
-        (this.field[6] === 'x' &&
-         this.field[7] === 'x' &&
-         this.field[8] === 'x')
+        (this.game.field[6] === 'x' &&
+         this.game.field[7] === 'x' &&
+         this.game.field[8] === 'x')
         ||
-        (this.field[0] === 'x' &&
-         this.field[4] === 'x' &&
-         this.field[8] === 'x')
+        (this.game.field[0] === 'x' &&
+         this.game.field[4] === 'x' &&
+         this.game.field[8] === 'x')
         ||
-        (this.field[2] === 'x' &&
-         this.field[4] === 'x' &&
-         this.field[6] === 'x')) {
-      this.playWon = 'x';
+        (this.game.field[2] === 'x' &&
+         this.game.field[4] === 'x' &&
+         this.game.field[6] === 'x')) {
+      this.game.playWon = 'x';
       return;
     }
 
-    if (this.field.filter(Boolean).length === 9) {
-      this.playWon = 'draw';
+    if (this.game.field.filter(Boolean).length === 9) {
+      this.game.playWon = 'draw';
     }
   }
 }
